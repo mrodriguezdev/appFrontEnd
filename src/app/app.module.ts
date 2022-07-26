4c6dce2e0592beaa683d5c033e0583b2f3b18f24
mrodriguezdev <marioguzman_95@hotmail.com>
2022-07-26 14:00:02 -0600

    Creacion de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { ActualizarComponent } from './views/actualizar/actualizar.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LoginComponent } from './views/login/login.component';
+import { NuevoComponent } from './views/nuevo/nuevo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ActualizarComponent,
+    DashboardComponent,
+    LoginComponent,
+    NuevoComponent
   ],
   imports: [
     BrowserModule,
